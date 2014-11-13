ac0c0822ac085c96c0867cc3d286c0d0a81367c7
Alex Eroshin <1425925@gmail.com>
2014-11-13 19:11:47 +0300

    endless road created

---
+++ Assets/Script/RoadMoveScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadMoveScript : MonoBehaviour {
+
+	private float z;
+
+	private float lastZ;
+
+	private Hashtable roadSegments = new Hashtable();
+
+	void Start () {
+		rigidbody.velocity = new Vector3 (0, 0, -120);
+//		GameObject[] gameObjects = FindObjectsOfType<GameObject> ();
+//		foreach (GameObject tempObject in gameObjects) {
+//			if (tempObject.name.Contains("Segment")) {
+//				roadSegments.Add (tempObject.transform.position.z, tempObject);
+//				print (tempObject.name);
+//			}
+//		}
+	}
+
+	void Update () {
+		z += transform.position.z - lastZ;
+		lastZ = transform.position.z;
+		if (z < -325) {
+			transform.position = new Vector3 (0, 152, -301);
+		}
+	}
+}
