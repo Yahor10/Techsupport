1767e6c4ade269c34f3955e2f59be322ade1a386
Alex Eroshin <1425925@gmail.com>
2014-11-17 16:58:33 +0300

    +obstacles

---
+++ Assets/Script/ObstacleMoveScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleMoveScript : MonoBehaviour {
+
+	public float speed = 120;
+
+	void Start () {
+		rigidbody.velocity = new Vector3 (0, 0, -speed);
+	}
+
+	void Update () {
+	
+	}
+}
