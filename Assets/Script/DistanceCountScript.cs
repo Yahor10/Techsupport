4075093a4546ada305cc2fd919bd6526d92e01c8
Alex Eroshin <1425925@gmail.com>
2014-11-18 20:18:15 +0300

    +distance count and health bar

---
+++ Assets/Script/DistanceCountScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistanceCountScript : MonoBehaviour {
+
+	private float distance = 0;
+
+	void Update () {
+		distance += Time.deltaTime * 10;
+		guiText.text = ((int) distance) + " meters";
+	}
+}
